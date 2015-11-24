7228455f68e5c599c497f7f97fc4988dfa840eb7
amitsaroj001 <amitnitrite005@outlook.com>
2015-11-24 22:33:53 +0530

    added

---
+++ HUBULLU.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int t;
+scanf("%d", &t);
+
+while(t--) {
+int n, p;
+scanf("%d""%d", &n, &p);
+printf("%s wins.\n", (p == 0)? "Airborne": "Pagfloyd");
+}
+return 0;
+}
